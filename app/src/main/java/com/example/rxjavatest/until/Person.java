68cd3c773665ed97de30ad0f44160efc7469f89b
chenlimin <472328291@qq.com>
2019-06-30 12:40:14 +0800

    哈哈哈

--- app/src/main/java/com/example/rxjavatest/until/Person.java
+++ app/src/main/java/com/example/rxjavatest/until/Person.java
@@ -1,4 +1,10 @@
 package com.example.rxjavatest.until;
 
+import android.content.SyncStatusObserver;
+import android.util.Log;
+
 public class Person {
+    public static void heh(){
+        Log.i("hhh","hahah");
+    }
 }
